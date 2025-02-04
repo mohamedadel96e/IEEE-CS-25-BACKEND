3973eed11508070ebb563df5d4ea256eeba11419
Mohamed Adel <mohamedadel96e@gmail.com>
2025-02-04 07:10:04 +0200

    First-Task-Done

---
+++ MySQL/Task_1.sql
@@ -0,0 +1,12 @@
+-- first Problem "Query all columns (attributes) for every row in the CITY table."
+
+SELECT * FROM CITY;
+
+
+-- second Problem "Query all columns for a city in CITY with the ID 1661."
+
+SELECT * FROM CITY WHERE ID = 1661;
+
+-- third Problem "Query a list of CITY and STATE from the STATION table."
+
+SELECT CITY, STATE FROM STATION;
